837ae21e53075060e6ff55f9d53605f6a93cd034
Themba <thembakmashele@gmail.com>
2025-06-05 12:09:24 +0200

    yeahh well

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io::Write;
 
 fn main() {
     println!("Guess the number!");
 
     
     // let secret_number = rand::thread_rng().gen_range(1..101);
-    let secret_number = rng::thread_rng().gen_range(1..101);
+    // let secret_number = rand::thread_rng().range(1..101);
+    let secret_number = rand::rng().random_range(1..101);
+
     // println!("The secret number is: {}", secret_number);
     
     loop {
-        println!("Please input your guess.");
+        print!("Please input your guess: ");
         let mut guess = String::new();
+
+        io::stdout().flush().expect("Failed to flush stdout");
 
         io::stdin().read_line(&mut guess).
         expect("Failed to read line");
