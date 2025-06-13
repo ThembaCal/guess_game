cbf3219e9e3772de4dec91f0396239a824fe8586
Themba <thembakmashele@gmail.com>
2025-06-13 19:22:28 +0200

    Added a counter to count the number of iterations

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use std::cmp::Ordering;
 use std::io::Write;
 
 fn main() {
+    println!();
     println!("Guess the number between 1 and 100 (inclusive).");
-
+    println!("__________________");
+    println!();
+    
     
     // let secret_number = rand::thread_rng().gen_range(1..101);
     // let secret_number = rand::thread_rng().range(1..101);
     let secret_number = rand::rng().random_range(1..101);
-
+    
     // println!("The secret number is: {}", secret_number);
     
+    let mut count = 0;
     loop {
+        
         print!("Please input your guess: ");
         let mut guess = String::new();
 
@@ -26,19 +31,27 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-
+        
         if guess < 1 || guess > 100 {
             println!("The secret number will be between 1 and 100.");
             continue;
         }
-    
-        println!("You gussed: {}", guess);
         
+        // println!("You gussed: {}", guess);
+        
+        count += 1;
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("Too small!");
+                println!("__________________");
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+                println!("__________________");
+            }, 
             Ordering::Equal => {
-                println!("You win!");
+                println!();
+                println!("You won!, in {count} tries");
                 break;
             } 
         }
